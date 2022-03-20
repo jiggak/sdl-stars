f033d4f3bee62055c9c9a86b1e35d44eeefeae68
Josh Kropf <josh@slashdev.ca>
2022-03-19 22:09:44 -0400

    add keyboard controls for rotation, zoom, scale

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 #define STAR_COUNT 1000
 #define STAR_SPREAD 5000
 
-#define SCALE 400
-#define DISTANCE 5000
+#define SCALE 600
+#define DISTANCE 4000
 
 #ifndef M_PI
    #define M_PI 3.14159265358979323846
@@ -66,15 +66,15 @@ void rotate_stars(Star *stars, int count, double pitch, double roll, double yaw)
 }
 
 // http://anthony.liekens.net/index.php/Computers/RenderingTutorial3DTo2D
-void project_stars(Star *stars, int count) {
+void project_stars(Star *stars, int count, int scale, int distance) {
    static float zmax = 0, zmin = 0;
 
    int xoff = SCREEN_WIDTH / 2;
    int yoff = SCREEN_HEIGHT / 2;
 
    for (int i = 0; i < count; i++) {
-      stars[i].dx = xoff + SCALE * stars[i].x / ( stars[i].z + DISTANCE );
-      stars[i].dy = yoff + SCALE * stars[i].y / ( stars[i].z + DISTANCE );
+      stars[i].dx = xoff + scale * stars[i].x / ( stars[i].z + distance );
+      stars[i].dy = yoff + scale * stars[i].y / ( stars[i].z + distance );
 
       zmax = fmax(zmax, stars[i].z);
       zmin = fmin(zmin, stars[i].z);
@@ -90,23 +90,20 @@ void render(SDL_Renderer *ctx) {
 
    SDL_bool running = SDL_TRUE;
    float yaw = 0.0f, pitch = 0.0f, roll = 0.0f;
+   int scale = SCALE, distance = DISTANCE;
 
    while (running) {
       rotate_stars(stars, STAR_COUNT, yaw, pitch, roll);
-      project_stars(stars, STAR_COUNT);
+      project_stars(stars, STAR_COUNT, scale, distance);
 
       for (int i = 0; i < STAR_COUNT; i++) {
-         SDL_SetRenderDrawColor(ctx, 0, stars[i].col, 0, 0xff);
+         SDL_SetRenderDrawColor(ctx, stars[i].col, stars[i].col, stars[i].col, 0xff);
          SDL_RenderDrawPoint(ctx, stars[i].dx, stars[i].dy);
       }
 
-      yaw = RAD;
-      //pitch = RAD;
-      roll = RAD;
-
       SDL_RenderPresent(ctx);
 
-      SDL_Delay(50);
+      SDL_Delay(1000/60);
 
       SDL_SetRenderDrawColor(ctx, 0, 0, 0, 0xff);
       SDL_RenderClear(ctx);
@@ -114,6 +111,37 @@ void render(SDL_Renderer *ctx) {
       SDL_Event event;
       while (SDL_PollEvent(&event)) {
          switch (event.type) {
+            case SDL_KEYUP:
+               switch (event.key.keysym.sym) {
+                  case SDLK_UP:
+                     pitch = pitch == 0 ? -RAD : 0;
+                     break;
+                  case SDLK_DOWN:
+                     pitch = pitch == 0 ? RAD : 0;
+                     break;
+                  case SDLK_LEFT:
+                     yaw = yaw == 0 ? RAD : 0;
+                     break;
+                  case SDLK_RIGHT:
+                     yaw = yaw == 0 ? -RAD : 0;
+                     break;
+                  case SDLK_1:
+                     distance += 100;
+                     break;
+                  case SDLK_2:
+                     distance -= 100;
+                     break;
+                  case SDLK_3:
+                     scale += 10;
+                     break;
+                  case SDLK_4:
+                     scale -= 10;
+                     break;
+               }
+
+               printf("distance %d scale %d\n", distance, scale);
+
+               break;
             case SDL_QUIT:
                running = SDL_FALSE;
                break;
